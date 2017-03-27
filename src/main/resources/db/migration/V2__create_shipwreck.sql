6e033a1da8bc59ca40151244d523f75b529c3d06
Wojtek <wojtasck1@o2.pl>
2017-03-27 15:18:25 +0200

    spring boot first app

---
+++ src/main/resources/db/migration/V2__create_shipwreck.sql
@@ -0,0 +1,10 @@
+CREATE TABLE SHIPWRECK(
+	id integer,
+	name varchar(40),
+	description varchar(40),
+	condition varchar(40),
+	depth integer,
+	latitude double precision,
+	longitude double precision,
+	year_Discovered integer 
+);
